cb2757262f9197c02996b7e023e956d90cb75a70
crazymoose77756 <52980616+crazymoose77756@users.noreply.github.com>
2021-06-12 15:55:26 -0400

    StarTool

---
+++ StarTool/src/me/crazymoose/StarTool/Main.java
@@ -0,0 +1,83 @@
+package me.crazymoose.StarTool;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+
+public class Main extends JavaPlugin implements Listener {
+	
+	
+	@Override
+	public void onEnable() {
+		this.getServer().getPluginManager().registerEvents(new StarEvents(this), this);
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		if(label.equalsIgnoreCase("startool")) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage("Bye console");
+				return true;
+			}
+			Player player = (Player) sender;
+			if (player.getInventory().firstEmpty() == -1) {
+				Location loc = player.getLocation();
+				World world = player.getWorld();
+				
+				world.dropItemNaturally(loc, getItem());
+				player.sendMessage(ChatColor.GOLD + "Dropped a gift near you");
+				return true;
+			}
+			player.getInventory().addItem(getItem());
+			player.sendMessage(ChatColor.GOLD + "Trident from the minecraft Gods");
+			return true;
+			
+		}
+		return false;
+	}
+	
+	
+	public ItemStack getItem () {
+		
+		ItemStack trident = new ItemStack(Material.TRIDENT);
+		ItemMeta meta = trident.getItemMeta();
+		
+		meta.setDisplayName(ChatColor.GREEN + "" + ChatColor.BOLD + "Boots of Leaping");
+		List<String> lore = new ArrayList<String>();
+		lore.add("");
+		lore.add(ChatColor.translateAlternateColorCodes('&', "&7(Right Click) &a&oSpawn minions"));
+		lore.add(ChatColor.translateAlternateColorCodes('&', "&7(Left Click) &a&oShoot explosives"));
+		meta.setLore(lore);
+		
+		meta.addEnchant(Enchantment.LOYALTY, 1, true);
+		meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+		meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+		meta.setUnbreakable(true);
+		
+		trident.setItemMeta(meta);
+		
+		return trident;
+	}
+	
+}
---
+++ StarTool/src/me/crazymoose/StarTool/StarEvents.java
@@ -0,0 +1,68 @@
+package me.crazymoose.StarTool;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Fireball;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class StarEvents implements Listener {
+	
+	public List<String> list = new ArrayList<String>();
+	
+	static Main plugin;
+	public StarEvents(Main instance) {
+		plugin = instance;
+	}
+	
+	
+
+		@EventHandler()
+		public void onClick(PlayerInteractEvent event) {
+			if (event.getPlayer().getInventory().getItemInMainHand().getType().equals(Material.TRIDENT))
+				if (event.getPlayer().getInventory().getItemInMainHand().getItemMeta().hasLore()) {
+					Player player = (Player) event.getPlayer();
+					if (event.getAction() == Action.RIGHT_CLICK_AIR) {
+						if (!list.contains(player.getName()))
+							list.add(player.getName());
+						return;
+					}
+					
+					if (event.getAction() == Action.LEFT_CLICK_AIR) {
+						player.launchProjectile(Fireball.class);
+					}
+				}
+			if (list.contains(event.getPlayer().getName())) {
+				list.remove(event.getPlayer().getName());
+			}
+		}
+		
+		@EventHandler()
+		public void onLand(ProjectileHitEvent event) {
+			if (event.getEntityType() == EntityType.TRIDENT) {
+				if (event.getEntity().getShooter() instanceof Player) {
+					Player player = (Player) event.getEntity().getShooter();
+					if (list.contains(player.getName())) {
+						Location loc = event.getEntity().getLocation();
+						loc.setY(loc.getY() + 1);
+						
+						for(int i =1; i < 4 ; i++) {
+							loc.getWorld().spawnEntity(loc, EntityType.DROWNED);
+							loc.setX(loc.getX() + i);
+						}
+					}
+				}
+			}
+		}
+		
+	}
+
+
